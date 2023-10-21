267849cbda11d9d128ba9dedcce72943f330613b
Viktor Lofgren <vlofgren@gmail.com>
2023-10-21 15:25:03 +0200

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker-compose -f wmsa_db.yml up -d
+docker-compose -f marginalia-core.yml up -d
+docker-compose -f marginalia-node-1.yml up -d
+docker-compose -f marginalia-node-2.yml up -d
+docker-compose -f marginalia-node-3.yml up -d
+docker-compose -f marginalia-node-4.yml up -d
+docker-compose -f marginalia-node-5.yml up -d
+docker-compose -f marginalia-node-6.yml up -d
+docker-compose -f marginalia-node-7.yml up -d
+docker-compose -f marginalia-node-8.yml up -d
